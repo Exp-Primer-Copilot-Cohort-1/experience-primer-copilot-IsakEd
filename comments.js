96fbf990071e8b2fe006142a40a24ed0e66ec5d7
Isak Ederlöv <isak.ederlov@gmail.com>
2023-10-13 11:37:40 +0200

    kinda works

---
+++ comments.js
@@ -0,0 +1,15 @@
+// Create a web server
+// Send back JSON data
+// GET, POST, DELETE, PUT
+
+// // Dependencies
+// var http = require('http');
+// var url = require('url');
+
+// // Configure our HTTP server to respond with Hello World to all requests.
+// var server = http.createServer(function (request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.end("Hello World\n");
+// });
+
+// // Listen on port 8000, IP defaults to
